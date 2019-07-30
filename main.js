ea73067ddc850c50c9e831ac9a6c0a3255cd66b3
Noah Gibson <noah.gibson99@gmail.com>
2019-07-30 17:40:04 -0600

    Add filter functionality

--- main.js
+++ main.js
@@ -33,6 +33,7 @@ function mainHandler() {
   urgent(event);
   checkTask(event);
   deleteCard(event);
+  promptList();
 }
 
 function taskButtonHandler() {
@@ -56,7 +57,7 @@ function clearAll() {
 }
 
 function filterHandler() {
-  if(filterButton.classList.value === '.aside__button-filter') {
+  if(filterButton.dataset.toggle === 'false') {
     filterOn();
   } else {
     filterOff();
@@ -304,17 +305,18 @@ function deleteCard(event) {
   var card = event.target.closest('.card');
   var cardDelete = event.target.closest('.card__delete');
   var cardIndex = findIndex(event);
+  if(cardDelete) {
   var arr = listArray[cardIndex].tasks.filter(task => task.completed === true);
-
+  
   if(cardDelete && arr.length === listArray[cardIndex].tasks.length) {
     card.remove();
     listArray[cardIndex].deleteFromStorage(cardIndex);
+    }
   }
-  promptList();
 }
 
 function promptList() {
-  if(listArray.length !== 0) {
+  if(listArray.length > 0) {
     prompt.style.display = 'none';
   } else {
     prompt.style.display = 'visible';
@@ -334,10 +336,19 @@ function filterOn() {
   filteredArray = listArray.filter(list => list.urgent === true);
   filteredArray.map(filteredTasks => newTask(filteredTasks, filteredTasks.tasks));
   filterButton.classList.add('aside__button-filter-toggle');
+  filterButton.dataset.toggle = true;
+  urgentMessage();
 }
 
 function filterOff() {
   main.innerHTML = '';
   persistToDos();
   filterButton.classList.remove('aside__button-filter-toggle');
+  filterButton.dataset.toggle = false;
+}
+
+function urgentMessage() {
+  if(main.innerHTML === '') {
+    main.insertAdjacentHTML('afterbegin', `<p class=urgent-prompt>Gotta have <span>yo'</span> self some urgency!</p>`)
+  }
 }
